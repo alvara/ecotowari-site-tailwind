e7e41e147876977a9804f750b1c2ee584144f806
Jesse Alvarado <contact@jessealvarado.com>
2023-02-10 23:33:16 +0900

    feat: create button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import { Typography } from './Typography';
 import mailboxImg from '../assets/mailbox-split.png';
+import Button from './buttons/Button';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className="bg-hero bg-contain bg-fixed">
-      <div className="bg-gradient-to-b from-primary to-secondary opacity-90">
+      <div className="bg-gradient-to-b from-primary/90 to-secondary/90">
         <div className="min-h-60 container mx-auto flex flex-col-reverse py-16 text-center lg:flex-row lg:text-left ">
-          <div className="flex-1 text-left lg:pr-16">
+          <div className="flex flex-1 flex-col justify-center gap-6 text-left lg:pr-16">
             <Typography
               variant="h1"
-              className="text-center text-white lg:text-left"
+              className="m-0 p-0 text-center text-white lg:text-left"
             >
               One sticker to reduce waste
             </Typography>
@@ -21,6 +23,11 @@ export default function Header() {
               Free your mailbox from unsolicited flyers with this real-life spam
               filter!
             </Typography>
+            <Link href={'/#sticker'}>
+              <Button variant="contained" className="mx-auto lg:mx-0">
+                Get a sticker
+              </Button>
+            </Link>
           </div>
           <div className="flex-1">
             <div className="mx-auto max-w-md lg:max-w-none">
