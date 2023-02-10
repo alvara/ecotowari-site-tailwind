2f4bb856fa49142ab96f11e48465cd53bde7f9e8
Jesse Alvarado <contact@jessealvarado.com>
2023-02-10 22:43:56 +0900

    feat: add google font to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import Footer from '../components/sections/Footer';
 import '../styles/global.css';
 import NavbarSimple from '../components/navbar/NavbarSimple';
+import { Nunito } from '@next/font/google';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={nunito.className}>
         <div className="flex min-h-screen flex-col items-stretch justify-between">
           <NavbarSimple />
           <div className="grow items-stretch">{children}</div>
