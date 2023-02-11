044ff01876209a2361172d52b55c1a5edb9aab0b
Jesse Alvarado <contact@jessealvarado.com>
2023-02-11 11:13:49 +0900

    feat: include border option

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -16,11 +16,19 @@ export interface ITypographyProps {
   lineBreak?: boolean;
   children: React.ReactNode;
   className?: string;
+  border?: boolean;
 }
 
 export const Typography: FC<
   ITypographyProps & HTMLAttributes<HTMLOrSVGElement>
-> = ({ variant, children, lineBreak = false, className, ...rest }) => {
+> = ({
+  variant,
+  children,
+  lineBreak = false,
+  border = false,
+  className,
+  ...rest
+}) => {
   // assign the correct tag based on the variant prop
   function assignTag() {
     if (
@@ -38,12 +46,16 @@ export const Typography: FC<
   const Tag = assignTag();
 
   // style the typography based on the variant prop
+  if (variant === 'h2') {
+    className = `${className} `;
+  }
+  // style the typography based on the variant prop
   if (variant === 'h3') {
     className = `${className} font-extrabold`;
   }
   // for underneath large headings (h1, h2, h3)
   if (variant === 'subtitle1') {
-    className = `${className}  font-semibold  text-2xl`;
+    className = `${className}  font-semibold`;
   }
   // for underneath small headings (card headings, etc.)
   if (variant === 'subtitle2') {
@@ -57,8 +69,13 @@ export const Typography: FC<
   }
 
   return (
-    <Tag className={`${Tag} ${lineBreak && 'mb-4'} ${className}`} {...rest}>
-      {children}
-    </Tag>
+    <>
+      <Tag className={`${Tag} ${lineBreak && 'mb-4'} ${className}`} {...rest}>
+        {children}
+      </Tag>
+      {border && (
+        <hr className=" relative -m-3 mx-auto w-32 border-t-4 border-primary p-5" />
+      )}
+    </>
   );
 };
