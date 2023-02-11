569334aa1206ab25fe4f08589ceba522dfe72b6d
Jesse Alvarado <contact@jessealvarado.com>
2023-02-11 14:32:13 +0900

    feat: implement trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '../components/sections/Footer';
 import '../styles/global.css';
 import NavbarSimple from '../components/navbar/NavbarSimple';
 import { Nunito } from '@next/font/google';
+import Drawer from '@/components/drawer/Drawer';
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -10,12 +11,14 @@ const nunito = Nunito({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <div className="flex min-h-screen flex-col items-stretch justify-between">
-          <NavbarSimple />
-          <div className="grow items-stretch">{children}</div>
-          <Footer />
-        </div>
+      <body className={`${nunito.className} `}>
+        <Drawer id="my-drawer">
+          <div className="flex min-h-screen flex-col items-stretch justify-between ">
+            <NavbarSimple />
+            <div className="grow items-stretch">{children}</div>
+            <Footer />
+          </div>
+        </Drawer>
       </body>
     </html>
   );
