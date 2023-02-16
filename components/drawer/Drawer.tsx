db0561da082faba0f690c3afae8da4cfbd46c860
Jesse Alvarado <contact@jessealvarado.com>
2023-02-17 01:29:38 +0900

    refactor: remove unused imports

--- components/drawer/Drawer.tsx
+++ components/drawer/Drawer.tsx
@@ -1,6 +1,4 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import Button from '../buttons/Button';
 import NavMenu from '../navbar/NavMenu';
 
